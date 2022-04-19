a8c53beb78b724960f06e19a80fddf0dcbb2a01b
Miasoutik <cedricwat@outlook.fr>
2022-04-20 00:03:23 +0200

    Fix bug btn

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const newBtn = document.getElementById("newGame");
-const roll = document.getElementById("roll");
-const hold = document.getElementById("hold");
+//const roll = document.getElementById("roll");
+//const hold = document.getElementById("hold");
+const rolls = document.getElementsByName("roll");
+const holds = document.getElementsByName("hold");
 const player1Names = document.getElementsByName("label1");
 const player2Names = document.getElementsByName("label2");
 const dots = document.getElementsByName("dot");
@@ -57,73 +59,77 @@ newBtn.addEventListener("click", () => {
   activePlayer(tour);
 });
 
-roll.addEventListener("click", () => {
-  let numb = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-  switch (numb) {
-    case 1:
-      img.src = "./images/dice-1.png";
-      if (tour === 1) {
-        joueur1.current = 0;
-        current1.innerHTML = 0;
-      } else {
-        joueur2.current = 0;
-        current2.innerHTML = 0;
-      }
-      tour = tour === 1 ? 2 : 1;
-      numb = 0;
-      activePlayer(tour);
-      break;
-    case 2:
-      img.src = "./images/dice-2.png";
-      break;
-    case 3:
-      img.src = "./images/dice-3.png";
-      break;
-    case 4:
-      img.src = "./images/dice-4.png";
-      break;
-    case 5:
-      img.src = "./images/dice-5.png";
-      break;
-    case 6:
-      img.src = "./images/dice-6.png";
-      break;
-  }
-  if (tour === 1) {
-    joueur1.current += numb;
-    current1.innerHTML = joueur1.current;
-  } else {
-    joueur2.current += numb;
-    current2.innerHTML = joueur2.current;
-  }
+rolls.forEach((roll) => {
+  roll.addEventListener("click", () => {
+    let numb = Math.floor(Math.random() * (6 - 1 + 1)) + 1;
+    switch (numb) {
+      case 1:
+        img.src = "./images/dice-1.png";
+        if (tour === 1) {
+          joueur1.current = 0;
+          current1.innerHTML = 0;
+        } else {
+          joueur2.current = 0;
+          current2.innerHTML = 0;
+        }
+        tour = tour === 1 ? 2 : 1;
+        numb = 0;
+        activePlayer(tour);
+        break;
+      case 2:
+        img.src = "./images/dice-2.png";
+        break;
+      case 3:
+        img.src = "./images/dice-3.png";
+        break;
+      case 4:
+        img.src = "./images/dice-4.png";
+        break;
+      case 5:
+        img.src = "./images/dice-5.png";
+        break;
+      case 6:
+        img.src = "./images/dice-6.png";
+        break;
+    }
+    if (tour === 1) {
+      joueur1.current += numb;
+      current1.innerHTML = joueur1.current;
+    } else {
+      joueur2.current += numb;
+      current2.innerHTML = joueur2.current;
+    }
+  });
 });
 
-hold.addEventListener("click", () => {
-  if (tour === 1) {
-    joueur1.score += joueur1.current;
-    joueur1.current = 0;
-    current1.innerHTML = 0;
-    score1.innerHTML = joueur1.score;
-    tour = 0;
-  } else {
-    joueur2.score += joueur2.current;
-    joueur2.current = 0;
-    current2.innerHTML = 0;
-    score2.innerHTML = joueur2.score;
-    tour = 1;
-  }
-  if (joueur1.score >= 100) {
-    player1Names.forEach((name) => {
-      gagne(name);
-    });
-    tour = 3;
-  } else if (joueur2.score >= 100) {
-    player2Names.forEach((name) => {
-      gagne(name);
-    });
-    tour = 3;
-  }
-  if (tour !== 3) {
-    activePlayer(tour);
-  }
+holds.forEach((hold) => {
+  hold.addEventListener("click", () => {
+    if (tour === 1) {
+      joueur1.score += joueur1.current;
+      joueur1.current = 0;
+      current1.innerHTML = 0;
+      score1.innerHTML = joueur1.score;
+      tour = 0;
+    } else {
+      joueur2.score += joueur2.current;
+      joueur2.current = 0;
+      current2.innerHTML = 0;
+      score2.innerHTML = joueur2.score;
+      tour = 1;
+    }
+    if (joueur1.score >= 100) {
+      player1Names.forEach((name) => {
+        gagne(name);
+      });
+      tour = 3;
+    } else if (joueur2.score >= 100) {
+      player2Names.forEach((name) => {
+        gagne(name);
+      });
+      tour = 3;
+    }
+    if (tour !== 3) {
+      activePlayer(tour);
+    }
+  });
 });
